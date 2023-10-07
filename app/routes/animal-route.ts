b833442fa73b4f008f97ac162b10ae81e7a4b616
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-10-06 23:33:55 -0300

    log

--- app/routes/animal-route.ts
+++ app/routes/animal-route.ts
@@ -3,12 +3,14 @@ import { prisma } from "../../lib/prisma-client";
 
 export async function handleAnimais(app: FastifyInstance) {
   app.get("/animal", async () => {
+    console.log("get animais");
     const animais = await prisma.animal.findMany();
 
     return { animais };
   });
 
   app.post("/animal", async (req: any, reply: FastifyReply) => {
+    console.log("criando animais");
     const { type, name, idade, porte, sexo, responsavel, adotado, userId } =
       req.body;
     return prisma.animal.create({
